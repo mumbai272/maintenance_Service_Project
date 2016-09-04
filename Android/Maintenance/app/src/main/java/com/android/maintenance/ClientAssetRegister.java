b112d1046c1516070784e4417540b777285732e3
anand <gcompany.hangandhave@gmail.com>
2016-09-04 16:23:32 +0530

    adding adroid project

---
+++ Android/Maintenance/app/src/main/java/com/android/maintenance/ClientAssetRegister.java
@@ -0,0 +1,13 @@
+package com.maintenance.maintenance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientAssetRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_asset_register);
+    }
+}
