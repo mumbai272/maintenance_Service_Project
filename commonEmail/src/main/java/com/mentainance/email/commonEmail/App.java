96dc00aa6c72fd7e4194f65681ffa559fdb28136
mumbai272 <vinayak.s.mumbai@gmail.com>
2016-10-02 03:46:43 +0530

    common mail

---
+++ commonEmail/src/main/java/com/mentainance/email/commonEmail/App.java
@@ -0,0 +1,13 @@
+package com.mentainance.email.commonEmail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
