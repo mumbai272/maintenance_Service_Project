da5aaf92403e4cd932f82a3ed42bdbb628b688fe
mumbai272 <vinayak.s.mumbai@gmail.com>
2016-11-24 01:48:29 +0530

    email

--- commonEmail/src/main/java/com/maintenance/email/EmailType.java
+++ commonEmail/src/main/java/com/maintenance/email/EmailType.java
@@ -7,22 +7,25 @@ package com.maintenance.email;
 public enum EmailType {
     ADD_USER_HTML_EMAIL("Add user", "html", "Invitation to login into Maintenance application",
             "/velocityTemplate/addUserHtml.vm"), 
-    FORGOT_PASSEORD_EMAIL("forgot password","html","Forgot password","/velocityTemplate/forgotUserPasswordHtml.vm"),
-    REGISTRATION_REJECTION("Registration rejected","html","Registration rejected","/velocityTemplate/registrationRejectedHtml.vm"),
-	    REGISTRATION_REQUEST("Registration request","html","Registration requested","/velocityTemplate/registrationRejectedHtml.vm"),
-;
+    FORGOT_PASSEORD_EMAIL("forgot password", "html",
+            "Forgot password", "/velocityTemplate/forgotUserPasswordHtml.vm"), 
+    REGISTRATION_REJECTION( "Registration rejected", "html", "Registration rejected",
+            "/velocityTemplate/registrationRejectedHtml.vm"), 
+    REGISTRATION_REQUEST("Registration request", "html", "Registration requested",
+            "/velocityTemplate/registrationRejectedHtml.vm");
 
     private String name;
 
     private String type;
+
     private String subject;
 
     private String velocityTempletFile;
 
-    private EmailType(String name, String type,String subject, String velocityTempletFile) {
+    private EmailType(String name, String type, String subject, String velocityTempletFile) {
         this.name = name;
         this.type = type;
-        this.subject=subject;
+        this.subject = subject;
         this.velocityTempletFile = velocityTempletFile;
     }
 
@@ -33,7 +36,7 @@ public enum EmailType {
     public String getType() {
         return type;
     }
-    
+
     public String getSubject() {
         return subject;
     }
